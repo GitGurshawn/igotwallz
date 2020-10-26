5427901feca42667ed0955f79824724f42e375a1
GitGurshawn <gurshawn98@gmail.com>
2020-10-26 00:06:56 -0700

    Remove import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import Map from './components/Map';
